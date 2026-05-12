f9d1b5297032647a2583546d046ca284114500fd
yeafemi <yeafemi@example.com>
2026-05-12 17:59:25 +0000

    fix: set router basepath to / for hash history

--- src/router.tsx
+++ src/router.tsx
@@ -11,7 +11,7 @@ export const getRouter = () => {
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
-    basepath: "/ignyte",
+    basepath: "/",
   });
 
   return router;
