5f4e08c91ba851d9af8b45153c8820438869d99f
yeafemi <yeafemi@example.com>
2026-05-12 18:08:45 +0000

    fix: revert to browser history and provide real supabase fallbacks

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter, createHashHistory } from "@tanstack/react-router";
+import { createRouter, createBrowserHistory } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
@@ -7,11 +7,11 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
-    history: createHashHistory(),
+    history: createBrowserHistory(),
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
-    basepath: "/",
+    basepath: "/ignyte",
   });
 
   return router;
